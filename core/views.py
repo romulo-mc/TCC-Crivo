5159221a97f377c9b3460a65d57ff6c0a8198400
Romulo MC <romulocardoso184@gmail.com>
2026-02-04 11:16:36 -0300

    feat(frontend): criar templates base e index listando recursos do banco

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Recurso
 from .serializers import RecursoSerializer
 
+
 class RecursoViewSet(viewsets.ModelViewSet):
     queryset = Recurso.objects.all()
     serializer_class = RecursoSerializer
+    
+def index(request):
+    recursos = Recurso.objects.all()
+    print ("Quantos filmes achei: ",recursos.count())
+    print("Primeiro filme: ",recursos.first())
+    context = {
+        'recursos': recursos
+    }
+    return render(request, 'index.html', context)
--- setup/urls.py
+++ setup/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # <--- IMPORTANTE: Adicione o include aqui
+from django.urls import path, include
+from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),  # <--- Essa linha conecta seu App na internet
+    path('api/', include('core.urls')),
+    path('', index, name='home'),
 ]
